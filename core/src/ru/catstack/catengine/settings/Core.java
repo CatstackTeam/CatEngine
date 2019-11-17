52b2f4885e7765593ef12c92f3fd0a65900dc5be
Gorr <gorrgorrob@gmail.com>
2019-11-17 16:53:36 +0500

    Versioning

--- core/src/ru/catstack/catengine/settings/Core.java
+++ core/src/ru/catstack/catengine/settings/Core.java
@@ -3,4 +3,5 @@ package ru.catstack.catengine.settings;
 public class Core {
     public static final float GAME_WIDTH = 720;
     public static final float GAME_HEIGHT = 1280;
+    public static final String ENGINE_VERSION = "0.1.0 pre-alpha";
 }
